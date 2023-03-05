8a0560d6a0e033d8c663015b54fe4aca8020dcad
Christopher Zorn <christopher.zorn@gmail.com>
2023-03-04 17:55:21 -0800

    zope.interface

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/interface.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/interface.py
@@ -4,7 +4,7 @@
 # consumer and producer. This has to be interchangable.
 # Is an agent interface class the way to go?
 # https://opensource.com/article/19/9/zopeinterface-python-package
-import zope
+import zope.interface
 
 # Is this overkill? We want an interface and possibly a registry.
 # https://zopeinterface.readthedocs.io/en/latest/adapter.html
